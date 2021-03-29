f75ee170f147188d0a68c65d9d9c909aa3ca1628
Ching-Wei <chingwei@Ethan-Macbook-Pro.local>
2021-03-29 23:10:32 +0800

    make it better

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+function wrong_answer() {
+    alert('笨笨 答錯了');
+}
+
+function q3awrong_answer() {
+    alert('笨笨 答錯了 這是我媽的電話');
+}
+
+function q3bwrong_answer() {
+    alert('笨笨 答錯了 這是我家電話');
+}
+
